842087f664c8a9c5008dcf9ce87b0d1f8034275b
monsalvechs <monsalvechs@gmail.com>
2021-08-25 11:33:03 -0300

    c

--- js/login.js
+++ js/login.js
@@ -1,1 +1,28 @@
-// js para la pagina del log in y reigstro del sitio
+document.addEventListener("DOMContentLoaded", function () {
+    
+    document.getElementById("sumitBtn").addEventListener("click", function () {
+    
+        let inputEmail = document.getElementById("inputEmail");
+        let inputPassword = document.getElementById("inputpassword");
+        let camposCompletos = true;
+
+        if (inputEmail.value === '') {
+            inputEmail.classList.add('invalid');
+            camposCompletos = false;
+        } else {
+            inputEmail.classList.remove("invalid");
+        }
+
+        if (inputPassword.value === '') {
+            inputPassword.classList.add('invalid');
+            camposCompletos = false;
+        } else {
+            inputPassword.classList.remove('invalid');
+        }
+
+        if (camposCompletos = true) {
+            window.location.href="index.html"
+        }
+            
+    });
+});
